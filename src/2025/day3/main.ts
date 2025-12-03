d4763104ddcf916c1059b0d20f315433c1eb2465
Rik Claessens <rik.claessens@nn-group.com>
2025-12-03 22:21:04 +0100

    aoc 2025 day 3 code improvements

--- src/2025/day3/main.ts
+++ src/2025/day3/main.ts
@@ -15,28 +15,46 @@ export const input = getInput(`src/${year}/${day}/input.txt`);
 const getBanks = (input: string[]): number[][] =>
   input.map((line) => line.split("").map((n) => parseInt(n)));
 
-const getHighestJoltage = (bank: number[], noOfBatteries: number): number => {
+const getHighestJoltageInRange = (
+  bank: number[],
+  from: number,
+  to: number,
+): [number, number] => {
+  let highestJoltage = 0;
+  let minIndex = from;
+  for (let j = from; j < to; j += 1) {
+    if (bank[j] > highestJoltage) {
+      highestJoltage = bank[j];
+      minIndex = j;
+    }
+  }
+  return [highestJoltage, minIndex];
+};
+
+const getHighestJoltageInBank = (bank: number[], noOfBatteries: number): number => {
   const joltages: number[] = Array.from({ length: noOfBatteries }, () => 0);
   let minIndex = 0;
 
   for (let i = 0; i < noOfBatteries; i += 1) {
-    for (let j = minIndex; j < bank.length - (noOfBatteries - i - 1); j += 1) {
-      if (bank[j] > joltages[i]) {
-        joltages[i] = bank[j];
-        minIndex = j;
-      }
-    }
-    minIndex += 1;
+    const [highestJoltage, newMinIndex] = getHighestJoltageInRange(
+      bank,
+      minIndex,
+      bank.length - (noOfBatteries - i - 1),
+    );
+    joltages[i] = highestJoltage;
+    minIndex = newMinIndex + 1;
   }
   return parseInt(joltages.join(""));
 };
 
+const sum = (total: number, joltage: number) => total + joltage;
+
 export const part1 = (input: string[]): number =>
   getBanks(input)
-    .map((b) => getHighestJoltage(b, 2))
-    .reduce((total, joltage) => total + joltage);
+    .map((b) => getHighestJoltageInBank(b, 2))
+    .reduce(sum);
 
 export const part2 = (input: string[]): number =>
   getBanks(input)
-    .map((b) => getHighestJoltage(b, 12))
-    .reduce((total, joltage) => total + joltage);
+    .map((b) => getHighestJoltageInBank(b, 12))
+    .reduce(sum);
